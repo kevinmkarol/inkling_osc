d659c2caeb0c0dd2b57847e9ac82421c4a3c83c6
Kevin Karol <kevinmkarol@gmail.com>
2016-02-15 19:07:46 -0500

    Num 1

--- configure.sh
+++ configure.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+#Required dependencies - if you know you already have this installed feel free to skip it
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
 git submodule init
 git submodule update
 
 cd src/libraries/hidapi
+cp hidapi/hidapi.h mac/hidapi.h
 ./bootstrap
 ./configure
 make
